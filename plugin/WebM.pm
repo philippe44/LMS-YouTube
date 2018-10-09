de7b711a8769bac796e1c38545dc5bf0489c68c1
philippe44 <philippe_44@outlook.com>
2018-10-09 10:02:46 -0700

    0.80.0.1

--- plugin/WebM.pm
+++ plugin/WebM.pm
@@ -560,7 +560,11 @@ sub decode_u8  { unpack('C', $_[0]) }
 sub decode_u16 { unpack('n', $_[0]) }
 sub decode_u24 { unpack('N', ("\0" . $_[0]) ) }
 sub decode_u32 { unpack('N', $_[0]) }
-sub decode_u64 { unpack('Q', $_[0]) }
+sub decode_u64 { 
+	return unpack('Q>', substr($_[0], 0, 8)) if $Config{ivsize} == 8;
+	$log->error("can't unpack 64 bits integer, using 32 bits LSB");
+	return unpack('N', substr($_[0], 4, 4));
+}
 sub decode_u { 
 	my ($s, $len) = @_;
 	return unpack('C', $_[0]) if ($len == 1);
@@ -568,7 +572,7 @@ sub decode_u {
 	return unpack('N', ("\0" . $_[0]) ) if ($len == 3);
 	return unpack('N', $_[0]) if ($len == 4);
 	if ($len == 8) {
-		#return unpack('Q', substr($_[0], 0, 8)) if $Config{ivsize} == 8;
+		return unpack('Q>', substr($_[0], 0, 8)) if $Config{ivsize} == 8;
 		$log->error("can't unpack 64 bits integer, using 32 bits LSB");
 		return unpack('N', substr($_[0], 4, 4));
 	} 
