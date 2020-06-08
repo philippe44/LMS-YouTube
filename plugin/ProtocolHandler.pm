fd4f9dce04909762dea2f9696571e99be0c129ed
Peter Oliver <git@mavit.org.uk>
2020-06-09 00:22:14 +0100

    Regexp handlers are set with new method registerURLHandler().

--- plugin/ProtocolHandler.pm
+++ plugin/ProtocolHandler.pm
@@ -54,7 +54,7 @@ my $prefs = preferences('plugin.youtube');
 my $cache = Slim::Utils::Cache->new;
 
 Slim::Player::ProtocolHandlers->registerHandler('youtube', __PACKAGE__);
-Slim::Player::ProtocolHandlers->registerHandler(PAGE_URL_REGEXP, __PACKAGE__);
+Slim::Player::ProtocolHandlers->registerURLHandler(PAGE_URL_REGEXP, __PACKAGE__);
 
 sub flushCache { $cache->cleanup(); }
 
